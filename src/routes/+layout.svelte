7477854d79e44441c2659753580134dbab50f9d9
duchiru <ngvit31082007@gmail.com>
2026-01-08 21:30:54 +0700

    feat: remove splash screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     import TitleBar from "../components/TitleBar.svelte";
     import { onMount } from "svelte";
+    import { getCurrentWindow } from "@tauri-apps/api/window";
 
     import "../app.css";
 
     onMount(async () => {
-        // Remove splash screen when the content is mounted
-        const splashScreen = document.getElementById("splash-screen");
-        if (splashScreen) splashScreen.remove();
+        // Show the window when the layout is mounted (to avoid white flash)
+        await getCurrentWindow().show();
     })
 </script>
 
